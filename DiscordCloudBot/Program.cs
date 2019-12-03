b000c534ca1062490139a5412f920111d7939983
Kirill Pogartsev <sprite96@ya.ru>
2019-12-03 18:05:14 +0500

    Первый коммит.

---
+++ DiscordCloudBot/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Threading.Tasks;
+using Discord;
+using Discord.WebSocket;
+
+namespace DiscordCloudBot
+{
+    public class Program
+    {
+        private DiscordSocketClient _client;
+
+        public static void Main(string[] args)
+        {
+            new Program().MainAsync().GetAwaiter().GetResult();
+        }
+
+        public async Task MainAsync()
+        {
+            _client = new DiscordSocketClient();
+            _client.Log += Log;
+            await _client.LoginAsync(TokenType.Bot,
+                Environment.GetEnvironmentVariable("DISCORD_TOKEN"));
+            await _client.StartAsync();
+            await Task.Delay(-1);
+        }
+
+
+        private Task Log(LogMessage message)
+        {
+            Console.WriteLine(message.ToString());
+            return Task.CompletedTask;
+        }
+    }
+}
---
+++ DiscordCloudBot/SpeechKitService.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Net.Http;
+using System.Threading.Tasks;
+
+namespace DiscordCloudBot
+{
+    public class SpeechKitService
+    {
+        public async Task<byte[]> SpeakAsync(string text)
+        {
+            var iamToken = Environment.GetEnvironmentVariable("YANDEX_IAM_TOKEN");
+            var folderId = Environment.GetEnvironmentVariable("YANDEX_FOLDER_ID");
+            using var client = new HttpClient();
+            client.DefaultRequestHeaders.Add("Authorization", "Bearer " + iamToken);
+            var values = new Dictionary<string, string>
+            {
+                {"text", text},
+                {"lang", "en-US"},
+                {"folderId", folderId}
+            };
+            var content = new FormUrlEncodedContent(values);
+            var response = await client.PostAsync(@"https://tts.api.cloud.yandex.net/speech/v1/tts:synthesize", content);
+            return await response.Content.ReadAsByteArrayAsync();
+        }
+    }
+}
